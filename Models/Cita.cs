097905724f833815f02f815efc2866ee049bbfbd
Alvarden <31715989+Alvarden15@users.noreply.github.com>
2018-10-11 12:12:01 -0500

    Agregando el EF Core

---
+++ Models/Cita.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace pangu.Models
+{
+    public class Cita{
+        [Required]
+        [MinLength(4)]
+        public string Nombre { get; set; }
+        [Required]
+        public string Mascota{get; set;}
+
+        [Required]
+        public int Telefono{get; set;}
+
+        public string TipoTelefono{get; set;}
+
+        public string Doctor{get; set;}
+        [Required]
+        public DateTime Fecha{get; set;}
+
+        public string TipoMascota{get; set;}
+        public string TipoCita{get; set;}
+        public string Direccion{get; set;}
+        
+    }
+}
--- Models/Persona.cs
+++ Models/Persona.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace pangu.Models
 {
